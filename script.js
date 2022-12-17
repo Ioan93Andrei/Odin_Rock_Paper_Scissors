a17cc161b6a9be153d2dc22c9d61ed9dd033e5e7
Ioan Andrei <ivan.andrei93@mail.ru>
2022-12-17 17:04:24 +0100

    set both computerChoice and playerChoice to lowercase to avoid problems in the future

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 let choices = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice(choices) {
-    let computerChoice = choices[Math.floor(Math.random() * choices.length)]
+    let computerChoice = choices[Math.floor(Math.random() * choices.length)].toLowerCase()
     return computerChoice;
 }
 
 function getPlayerChoice() {
-    let playerChoice = prompt("Please enter your choice from Rock, Paper, Scissors: ");
+    let playerChoice = prompt("Please enter your choice from Rock, Paper, Scissors: ").toLowerCase();
     return playerChoice;
 }
 
-let PlayerChoice = getPlayerChoice();
+let playerChoice = getPlayerChoice();
 let computerChoice = getComputerChoice(choices);
