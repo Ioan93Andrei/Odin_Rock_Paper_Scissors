415712223dc7186c8f3ede71103942b1094b78a3
Ioan Andrei <ivan.andrei93@mail.ru>
2022-12-17 17:31:30 +0100

    the main function playRound is working.

--- script.js
+++ script.js
@@ -10,5 +10,48 @@ function getPlayerChoice() {
     return playerChoice;
 }
 
-let playerChoice = getPlayerChoice();
+// Runs the computer and player choices.
+
 let computerChoice = getComputerChoice(choices);
+let playerChoice = getPlayerChoice();
+
+
+// Starts a round of the game.
+
+function playRound(playerChoice, computerChoice){
+
+  switch(playerChoice){
+
+    case "rock":
+        if(computerChoice === "rock") { 
+            console.log(`Tie, both of you chose ${computerChoice}.`);
+        } else if (computerChoice === "paper") {
+            console.log(`You lose ${computerChoice} beats ${playerChoice}.`);
+        } else {
+            console.log(`You win ${playerChoice} beats ${computerChoice}.`);
+        }
+    break;
+
+    case "paper":
+        if(computerChoice === "rock") { 
+            console.log(`You win ${playerChoice} beats ${computerChoice}.`);
+        } else if (computerChoice === "paper") {
+            console.log(`Tie, both of you chose ${computerChoice}.`);
+        } else  {
+            console.log(`You lose ${computerChoice} beats ${playerChoice}.`);
+        }
+    break;
+
+    case "scissors":
+        if(computerChoice === "rock") { 
+            console.log(`You lose ${computerChoice} beats ${playerChoice}.`);
+        } else if (computerChoice === "paper") {
+            console.log(`You win ${playerChoice} beats ${computerChoice}.`);
+        } else {
+            console.log(`Tie, both of you chose ${computerChoice}.`);
+        }
+    break;
+  }
+}
+
+playRound(playerChoice, computerChoice);
